82f8dcaef51220b94b254b06e84e74263916b138
Oleh Filonenko <olegfill0007@gmail.com>
2024-09-25 21:48:42 +0300

    Create Lab1.cpp

---
+++ Create Lab1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int number = 7;
+
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << number << " * " << i << " = " << number * i << std::endl;
+    }
+
+    return 0;
+}
